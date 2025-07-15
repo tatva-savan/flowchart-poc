d5466c9ad316b532339df02a8120d2b055188602
Nikhil Vaya <nikhil.vaya@tatvasoft.com>
2025-07-15 19:07:57 +0530

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import { CanvasHandle, FlowCanvas } from "./FlowCanvas";
+import { ShapeButtons } from "./components/ShapeButtons";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
+  const canvasRef = useRef<CanvasHandle>(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", height: "100vh" }}>
+      <Sidebar />
+      <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+        <ShapeButtons onAdd={(t) => canvasRef.current?.addShape(t)} />
+        <FlowCanvas ref={canvasRef} />
+      </div>
     </div>
   );
 }
